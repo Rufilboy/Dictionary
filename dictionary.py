f106877f50a384fe058e5ae9fe07158d70106959
rufilboy <rufaiilyas600@gmail.com>
2021-12-19 14:14:55 +0100

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -12,16 +12,16 @@ def translate(word):
     elif word.upper() in data:
         return data[word.upper()]
     elif len(get_close_matches(word , data.keys())) > 0 :
-        print("did you mean %s instead" %get_close_matches(word, data.keys())[0])
-        decide = input("press y for yes or n for no")
+        print("Did you mean %s instead" %get_close_matches(word, data.keys())[0])
+        decide = input("Press Y for yes or N for no: ")
         if decide == "y":
             return data[get_close_matches(word , data.keys())[0]]
         elif decide == "n":
-            return("pugger your paw steps on wrong keys ")
+            return("Pugger your paw steps on wrong keys. ")
         else:
-            return("You have entered wrong input please enter just y or n")
+            return("You have entered wrong input, unable to locate your search.")
     else:
-        print("pugger your paw steps on wrong keys")
+        print("Pugger your paw steps on wrong keys")
 
 
 
